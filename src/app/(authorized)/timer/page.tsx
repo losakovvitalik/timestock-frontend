4ca7276c298630b2621d20c425628a901147d99e
losakovvitalik <losakovgithub@gmail.com>
2025-05-01 12:28:15 +0300

    feat(start-stop-timer): добавил компонент таймера

--- src/app/(authorized)/timer/page.tsx
+++ src/app/(authorized)/timer/page.tsx
@@ -1,10 +1,14 @@
-import { StartTimerButton } from '@/features/start-timer/ui/start-timer-button';
+import { StartTimerButton } from '@/features/start-stop-timer/ui/start-timer-button';
+import { Timer } from '@/features/start-stop-timer/ui/timer';
 
 export default function TimerPage() {
   return (
     <div className="grid h-full grid-rows-2">
-      <div className="flex items-center justify-center">
-        <StartTimerButton />
+      <div className="flex items-center justify-center border-b-2 border-border">
+        <div className="flex flex-col items-center gap-4">
+          <StartTimerButton />
+          <Timer />
+        </div>
       </div>
     </div>
   );
---
+++ src/features/start-stop-timer/ui/timer.tsx
@@ -0,0 +1,7 @@
+import Typography from '@/shared/ui/typography';
+
+function Timer() {
+  return <Typography variant={'subtitle'}>Старт</Typography>;
+}
+
+export { Timer };

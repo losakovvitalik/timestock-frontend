00a99e76ef23b796674631742d7f55a1155bd41e
losakovvitalik <losakovgithub@gmail.com>
2025-05-03 12:53:41 +0300

    chore(eslint): :technologist: обновил правила eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next', 'prettier'),
+  {
+    ignores: ['src/shared/types/generated/'],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   ...pluginQuery.configs['flat/recommended'],
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;

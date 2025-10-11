a1c370167e402ae94d82ccf6dec03489e881a84d
Виталий <losakovgithub@gmail.com>
2025-10-11 16:37:48 +0300

    feature(auth): добавил кнопку возврата к вводу email в форме код

--- src/app/auth/code/page.tsx
+++ src/app/auth/code/page.tsx
@@ -1,6 +1,8 @@
 import { AuthOTPForm } from '@/features/auth/ui/auth-otp-form';
 import { paths } from '@/shared/constants';
 import { PageProps } from '@/shared/types/next';
+import { Button } from '@/shared/ui/button';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default async function AuthCodePage(props: PageProps<any, { email: string }>) {
@@ -14,6 +16,9 @@ export default async function AuthCodePage(props: PageProps<any, { email: string
   return (
     <div className="flex h-full flex-col items-center justify-center gap-2 px-4">
       <AuthOTPForm email={email} />
+      <Button className="rounded-full" asChild variant="outline">
+        <Link href={paths.auth.link}>Изменить email</Link>
+      </Button>
     </div>
   );
 }
--- src/shared/constants/paths.ts
+++ src/shared/constants/paths.ts
@@ -15,4 +15,4 @@ export const paths = {
     link: '/auth',
     code: '/auth/code',
   },
-};
+} as const;

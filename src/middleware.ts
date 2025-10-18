38b0153c6938d3e0cb388ae0e5c1d0e274b4ff69
Виталий <losakovgithub@gmail.com>
2025-10-18 15:46:47 +0300

    fix(auth): доработал редирект

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,19 @@ import { paths } from '@/shared/constants';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-// Маршруты, куда пускаем без проверки
-const PUBLIC_PATHS = [paths.auth.link, paths.auth.code];
-
-function isPublic(pathname: string) {
-  return PUBLIC_PATHS.some((p) => pathname.startsWith(p));
-}
-
 export function middleware(req: NextRequest) {
   const { pathname, origin } = req.nextUrl;
 
-  if (isPublic(pathname)) return NextResponse.next();
-
+  const isAuthRoute = pathname === paths.auth.link || pathname === paths.auth.code;
   const hasRefresh = req.cookies.get('strapi_up_refresh');
 
-  if (!hasRefresh) {
+  if (!hasRefresh && !isAuthRoute) {
     const url = req.nextUrl.clone();
     url.pathname = paths.auth.link;
     return NextResponse.redirect(url);
   }
 
-  const isAuthRoute = pathname === paths.auth.link || pathname === paths.auth.code;
-
+  console.log(pathname, isAuthRoute, hasRefresh);
   if (hasRefresh && isAuthRoute) {
     return NextResponse.redirect(new URL(paths.timer, origin));
   }
